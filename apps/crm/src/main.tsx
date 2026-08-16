5f42aedfe35898008416550327f402ac0b78b818
Olim Tursunov <olimtoy83@gmail.com>
2026-08-17 02:46:23 +0500

    feat(ui): establish design foundation

--- apps/crm/src/main.tsx
+++ apps/crm/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import '@madina/ui'
 import './index.css'
 import './App.css'
 import { router } from './app/router/index'
