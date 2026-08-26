79bf6713a03883db301d32004a9f960d172a9483
Olim Tursunov <olimtoy83@gmail.com>
2026-08-26 15:29:23 +0500

    feat(server): add api v1 root endpoint

--- apps/server/src/routes/api/v1/index.ts
+++ apps/server/src/routes/api/v1/index.ts
@@ -1,3 +1,10 @@
+import type { ApiV1Response } from '@madina/api'
 import type { FastifyInstance } from 'fastify'
 
-export async function apiV1Routes(_app: FastifyInstance) {}
+export async function apiV1Routes(app: FastifyInstance) {
+  app.get('/', async (): Promise<ApiV1Response> => {
+    return {
+      version: 'v1',
+    }
+  })
+}
